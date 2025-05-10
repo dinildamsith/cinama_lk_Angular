4200d0e4c3a7d3f724911426ceca6372b605bd4c
dinildamsith <dinildamsith52@gmail.com>
2025-05-10 23:39:28 +0530

    changes apply css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {MovieComponent} from '../page/movies/movie.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: MovieComponent}
+];
