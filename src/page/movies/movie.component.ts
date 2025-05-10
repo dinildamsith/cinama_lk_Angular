451569f2ebf1472722b9c6ed38944446606d6def
dinildamsith <dinildamsith52@gmail.com>
2025-05-11 00:21:55 +0530

    movie all show page done

--- src/page/movies/movie.component.ts
+++ src/page/movies/movie.component.ts
@@ -1,15 +1,59 @@
 import {Component} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgForOf,
+    NgIf
   ],
   standalone: true
 })
 
 export class MovieComponent {
+
+  movies = [
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      image: 'https://i5.walmartimages.com/seo/Avengers-Infinity-War-Movie-Poster-Print-Regular-Style-Size-24-X-36_65e23347-2ccc-4581-9700-581e0ea9c3a8.a808f8889bfa9e368659fbefc5e5dda4.jpeg',
+      rating: 9.3,
+      adult: false,
+    },
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      image: 'https://i5.walmartimages.com/seo/Avengers-Infinity-War-Movie-Poster-Print-Regular-Style-Size-24-X-36_65e23347-2ccc-4581-9700-581e0ea9c3a8.a808f8889bfa9e368659fbefc5e5dda4.jpeg',
+      rating: 9.3,
+      adult: false,
+    }
+    ,
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      image: 'https://i5.walmartimages.com/seo/Avengers-Infinity-War-Movie-Poster-Print-Regular-Style-Size-24-X-36_65e23347-2ccc-4581-9700-581e0ea9c3a8.a808f8889bfa9e368659fbefc5e5dda4.jpeg',
+      rating: 1.3,
+      adult: false,
+    },
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      image: 'https://i5.walmartimages.com/seo/Avengers-Infinity-War-Movie-Poster-Print-Regular-Style-Size-24-X-36_65e23347-2ccc-4581-9700-581e0ea9c3a8.a808f8889bfa9e368659fbefc5e5dda4.jpeg',
+      rating: 3.3,
+      adult: false,
+    },
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      image: 'https://i5.walmartimages.com/seo/Avengers-Infinity-War-Movie-Poster-Print-Regular-Style-Size-24-X-36_65e23347-2ccc-4581-9700-581e0ea9c3a8.a808f8889bfa9e368659fbefc5e5dda4.jpeg',
+      rating: 10,
+      adult: false,
+    }
+  ];
+
+  constructor() {
+  }
 
 }
