2ae68fc10ad778254638ae9f2ba240d8bda5448f
dinildamsith <dinildamsith52@gmail.com>
2025-05-11 23:00:09 +0530

    movie details page almost done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {MovieComponent} from '../page/movies/movie.component';
 import {TvShowsComponent} from '../page/tvShows/tvShows.component';
 import {PeopleComponent} from '../page/people/people.component';
+import {MovieDetailsComponent} from '../page/movieDetilsPage/movie-details.component';
 
 export const routes: Routes = [
   {path:'', component: MovieComponent},
   {path:'tv-shows', component: TvShowsComponent},
-  {path:'people', component: PeopleComponent}
+  {path:'people', component: PeopleComponent},
+  {path: 'movie-details', component: MovieDetailsComponent}
 ];
---
+++ src/page/movieDetilsPage/movie-details.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  standalone: true,
+  imports: [],
+})
+
+export class MovieDetailsComponent {
+
+
+}
