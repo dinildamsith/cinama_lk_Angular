e36e96e1da10ffaab13adc4fbbe270addb572f42
dinildamsith <dinildamsith52@gmail.com>
2025-05-12 12:55:22 +0530

    tv show details page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {MovieComponent} from '../page/movies/movie.component';
 import {TvShowsComponent} from '../page/tvShows/tvShows.component';
 import {PeopleComponent} from '../page/people/people.component';
 import {MovieDetailsComponent} from '../page/movieDetilsPage/movie-details.component';
+import {TvShowDetailsComponent} from '../page/tvShowDetilsPage/tv-show-details.component';
 
 export const routes: Routes = [
   {path:'', component: MovieComponent},
   {path:'tv-shows', component: TvShowsComponent},
   {path:'people', component: PeopleComponent},
-  {path: 'movie-details', component: MovieDetailsComponent}
+  {path: 'movie-details', component: MovieDetailsComponent},
+  {path: 'tv-show-details', component: TvShowDetailsComponent}
 ];
---
+++ src/page/tvShowDetilsPage/tv-show-details.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-tv-show-details',
+  templateUrl: './tv-show-details.component.html',
+  standalone: true,
+  imports: [],
+})
+
+export class TvShowDetailsComponent {
+
+
+}
