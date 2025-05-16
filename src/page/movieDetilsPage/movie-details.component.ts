00aeb3118a49fc93e23402a9dc441631c739d418
dinildamsith <dinildamsith52@gmail.com>
2025-05-16 19:29:53 +0530

    selected movie get api connected and set movie details

--- src/page/movieDetilsPage/movie-details.component.ts
+++ src/page/movieDetilsPage/movie-details.component.ts
@@ -1,13 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieServices} from '../../services/movie.services';
+import {ActivatedRoute} from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
 })
 
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit{
 
+
+  constructor(private movieService: MovieServices, private route: ActivatedRoute) {}
+
+  loading: boolean = false;
+  movieDetails: any = {}
+
+
+  //------------selected movie get by id
+  selectedMovieGet(movieId: any){
+    this.loading = true
+
+    this.movieService.getSelectedMovieGet(movieId).subscribe({
+      next : (res : any) => {
+        console.log(res)
+        this.movieDetails = res
+        this.loading = false
+      }, error: (err: any) => {
+        console.log(err)
+        this.loading = false
+      }
+    })
+  }
+
+
+  ngOnInit(): void {
+    const movieId = this.route.snapshot.paramMap.get('movieId');
+    console.log(movieId)
+    this.selectedMovieGet(movieId)
+  }
 
 }
--- src/services/endpoint.list.ts
+++ src/services/endpoint.list.ts
@@ -2,3 +2,5 @@ export const NOW_PLAYING_ALL_MOVIES_GET_URL = "movie/now_playing?language=en-US&
 export const POPULAR_ALL_MOVIES_GET_URL = "movie/popular?language=en-US&page="
 export const TOP_RATED_ALL_MOVIES_GET_URL = "movie/top_rated?language=en-US&page="
 export const UPCOMING_ALL_MOVIES_GET_URL = "movie/upcoming?language=en-US&page="
+
+export const SELECTED_MOVIE_DETAILS_GET_URL = "movie/"
