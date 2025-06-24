b0e89d31e23e2ea548b098be812174d957063616
dinildamsith <dinildamsith52@gmail.com>
2025-06-24 21:00:36 +0530

    CHANGES

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // --------- local development environment configuration ---------
 export const environment = {
   production: false,
-  tmdbToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjEzNmZkNjc2MTNlY2RiYjY4MDI2MzdmNjIzZWFmOCIsIm5iZiI6MTc0NjcyODQ2Ny4yNzgsInN1YiI6IjY4MWNmNjEzMzhkNTEyZWZhZGIxY2FhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OPQgXPjc5CbMOYxxfZ1aYimZCbGbfpwmavzep_tDvd0'
+  tmdbToken: 'YOUR_TMDB_API'
 };
